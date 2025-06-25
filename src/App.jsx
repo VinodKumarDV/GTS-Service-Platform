eb9bb79304ca165b9e4f33a7f953f59915f1b948
Vinodkumar D V <dvvinod143@gmail.com>
2025-06-25 19:04:00 +0530

    Add main.jsx and App.jsx as app entry point and root component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ClientStats from './components/ClientStats';
+import './styles/main.css';
+
+function App() {
+  return (
+    <main className="container">
+      <ClientStats />
+    </main>
+  );
+}
+
+export default App;
