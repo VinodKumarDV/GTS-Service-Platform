0f56272312a9b7f42526c55a8b648c5d0a9f6ec6
Vinodkumar D V <dvvinod143@gmail.com>
2025-06-26 16:45:55 +0530

    Update some issue with the spacing

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -13,22 +13,22 @@ export default function FilterBar({ clients }) {
 
   return (
     <div className="filter-bar" style={{marginBottom:'1rem'}}>
-      <select value={f.industry} onChange={e=>dispatch(setIndustry(e.target.value))}>
+      <select style={{marginBottom:'0.5rem'}} value={f.industry} onChange={e=>dispatch(setIndustry(e.target.value))}>
         <option value="">All Industries</option>
         {industries.map(ind=><option key={ind}>{ind}</option>)}
       </select>
 
-      <select value={f.location} onChange={e=>dispatch(setLocation(e.target.value))}>
+      <select style={{marginBottom:'0.5rem'}} value={f.location} onChange={e=>dispatch(setLocation(e.target.value))}>
         <option value="">All Locations</option>
         {locations.map(loc=><option key={loc}>{loc}</option>)}
       </select>
 
-      <select value={f.subscription} onChange={e=>dispatch(setSubscription(e.target.value))}>
+      <select style={{marginBottom:'0.5rem'}} value={f.subscription} onChange={e=>dispatch(setSubscription(e.target.value))}>
         <option value="">All Tiers</option>
         {tiers.map(tier=><option key={tier}>{tier}</option>)}
       </select>
 
-      <button onClick={()=>dispatch(clearFilters())}>Reset</button>
+      <button style={{marginBottom:'0.5rem'}} onClick={()=>dispatch(clearFilters())}>Reset</button>
     </div>
   );
 }
